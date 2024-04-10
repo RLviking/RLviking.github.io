65d7f088fd22467456bad9647ffcc1925a277adc
RLviking <111746471+RLviking@users.noreply.github.com>
2024-04-10 14:41:21 +0200

    Co-authored-by: NejTillKatter <NejTillKatter@users.noreply.github.com>

---
+++ ja.js
@@ -0,0 +1,27 @@
+document.getElementsByTagName("header")[0].innerHTML = `
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Powerfit Gym</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Hem</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="prislista.html">Prislista</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="medier.html">Social media</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="omoss.html">Om oss</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kontakt.html">Kontakta oss</a>
+                  </li>
+              </ul>
+            </div>
+          </nav>
+`;
