423d7b84b2653779189ccf942b5c6cebe384b62a
RLviking <111746471+RLviking@users.noreply.github.com>
2024-05-06 09:53:45 +0200

    Co-authored-by: NejTillKatter <NejTillKatter@users.noreply.github.com>

--- ja.js
+++ ja.js
@@ -1,10 +1,10 @@
 document.getElementsByTagName("header")[0].innerHTML = `
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Powerfit Gym</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand">Powerfit Gym</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarNav"> 
               <ul class="navbar-nav">
                 <li class="nav-item active">
                   <a class="nav-link" href="index.html">Hem</a>
@@ -21,4 +21,13 @@ document.getElementsByTagName("header")[0].innerHTML = `
               </ul>
             </div>
           </nav>
+`;
+
+
+document.getElementsByTagName("footer")[0].innerHTML = `
+<footer class ="footer-dark bg-dark">
+<h6>Kontakta gärna oss med frågor:</h6>
+<span>Mail: info@powerfitgym.com</span>
+<span>Telefon: 123-456789</span>
+</footer>
 `;
